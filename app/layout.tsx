a4776ace22e866a57ac79ec0c6c7a8fe085c4116
KhalidEchchahid <khalid.echchahid@usmba.ac.ma>
2025-03-20 23:46:16 +0000

    new pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '823564859732119');
+          fbq('init', '6640555696069277');
           fbq('track', 'PageView');
         `}
         </Script>
@@ -61,7 +61,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=823564859732119&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=6640555696069277&ev=PageView&noscript=1`}
           />
         </noscript>
       </body>
