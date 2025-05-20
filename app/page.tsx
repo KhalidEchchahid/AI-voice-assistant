97c2036d537148b4687eb8341359acff78fe5792
KhalidEchchahid <khalid.echchahid@usmba.ac.ma>
2025-05-20 15:44:21 +0100

    resize

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import AssistantToggle from "@/components/assistant-toggle";
-import VoiceAssistant from "@/components/voice-assistant";
 
 export default function Home() {
   return (
-    <main className="flex h-[100vh] items-center justify-center bg-transparent">
-      <div className="w-full h-full">
-        <VoiceAssistant />
-      </div>
+    <main className="flex items-center justify-center bg-transparent">
+      <AssistantToggle />
     </main>
   );
 }
