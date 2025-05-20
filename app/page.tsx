ef147e7fdd666928496f74eb85f28d94fe101bf1
KhalidEchchahid <khalid.echchahid@usmba.ac.ma>
2025-05-20 15:29:35 +0100

    return back

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import AssistantToggle from "@/components/assistant-toggle"
+import AssistantToggle from "@/components/assistant-toggle";
+import VoiceAssistant from "@/components/voice-assistant";
 
 export default function Home() {
   return (
-    <main className="bg-transparent">
-      <AssistantToggle />
+    <main className="flex h-[100vh] items-center justify-center bg-transparent">
+      <div className="w-full h-full">
+        <VoiceAssistant />
+      </div>
     </main>
-  )
+  );
 }
